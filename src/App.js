0105e2038a18728de74a123fe1a6b8eeca95ba3e
Praz <t.prazwal@gmail.com>
2021-04-22 23:25:46 +0200

    add materialUI core and icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <h1>Lets build gmail</h1>
     </div>
   );
