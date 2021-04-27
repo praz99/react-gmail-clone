08b3fc84158d81826d3fb6d1e0435b9752ba9e1c
Praz <t.prazwal@gmail.com>
2021-04-27 22:15:18 +0200

    Add and style the header left.

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton } from '@material-ui/core';
 
 function Header() {
-  return <div className="header">
-    <h2>This is a header</h2>
-  </div>
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="..." />
+      </div>
+
+      <div className="header__middle">
+        
+      </div>
+
+      <div className="header__right">
+        
+      </div>
+    </div>
+  );
 }
 
 export default Header;
