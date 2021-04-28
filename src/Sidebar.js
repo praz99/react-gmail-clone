f595d2a125c8d783fd1b0ff45ba8e0202cefdfae
Praz <t.prazwal@gmail.com>
2021-04-28 02:01:03 +0200

    Add the Sidebar component Add compose button in sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <Button startIcon={<AddIcon fontsize="large" className="sidebar__compose" />}>Compose</Button>
+    </div>
+  );
+}
+
+export default Sidebar;
