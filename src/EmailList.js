aedc607becf483d0a772500dc2e8b4f24618c5d6
Praz <t.prazwal@gmail.com>
2021-04-29 23:33:00 +0200

    add EmailList top right icons

--- src/EmailList.js
+++ src/EmailList.js
@@ -3,6 +3,10 @@ import { IconButton, Checkbox } from '@material-ui/core';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import RedoIcon from '@material-ui/icons/Redo';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
 import './EmailList.css';
 
 function EmailList() {
@@ -19,6 +23,20 @@ function EmailList() {
           </IconButton>
           <IconButton>
             <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
           </IconButton>
         </div>
       </div>
