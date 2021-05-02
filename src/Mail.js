294a7cc4d5f3562c0845c0677bbe5e6d6c0439c5
Praz <t.prazwal@gmail.com>
2021-05-02 23:59:39 +0200

    add tools icons to the top left mail body.

--- src/Mail.js
+++ src/Mail.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { ArrowBack } from '@material-ui/icons';
+import { IconButton } from '@material-ui/core';
 import './Mail.css';
 
 function Mail() {
   return (
     <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton onClick={() => history.push('/')}>
+            <ArrowBack />
+          </IconButton>
+        </div>
+        <div className="mail__toolsRight">
 
+        </div>
+      </div>
     </div>
   );
 }
