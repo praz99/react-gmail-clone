8257796cb0a12baa30acec2afd9792a91acee2d6
Praz <t.prazwal@gmail.com>
2021-05-03 00:05:21 +0200

    add more icons to the top left mail body.

--- src/Mail.js
+++ src/Mail.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { ArrowBack } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
+import { ArrowBack, CheckCircle, Delete, Email, Error, LabelImportant, MoreVert, MoveToInbox, WatchLater } from '@material-ui/icons';
 import { IconButton } from '@material-ui/core';
 import './Mail.css';
 
 function Mail() {
+  const history = useHistory();
   return (
     <div className="mail">
       <div className="mail__tools">
         <div className="mail__toolsLeft">
           <IconButton onClick={() => history.push('/')}>
             <ArrowBack />
+          </IconButton>
+          <IconButton>
+            <MoveToInbox />  
+          </IconButton>
+          <IconButton>
+            <Error />
+          </IconButton>
+          <IconButton>
+            <Delete />
+          </IconButton>
+          <IconButton>
+            <Email />
+          </IconButton>
+          <IconButton>
+            <WatchLater />
+          </IconButton>
+          <IconButton>
+            <CheckCircle />
+          </IconButton>
+          <IconButton>
+            <LabelImportant />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
           </IconButton>
         </div>
         <div className="mail__toolsRight">
