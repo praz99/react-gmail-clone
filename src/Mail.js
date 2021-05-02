103aafe85c0473dfdf6ffe37d42e0249f8d86a06
Praz <t.prazwal@gmail.com>
2021-05-03 00:30:18 +0200

    add and style the mail body.

--- src/Mail.js
+++ src/Mail.js
@@ -50,6 +50,17 @@ function Mail() {
           </IconButton>
         </div>
       </div>
+      <div className="mail__body">
+        <div className="mail__bodyHeader">
+          <h2>Subject</h2>
+          <LabelImportant className="mail__important" />
+          <p>Title</p>
+          <p className="mail__time">10pm</p>
+        </div>
+        <div className="mail__message">
+          <p>This is a message.</p>
+        </div>
+      </div>
     </div>
   );
 }
