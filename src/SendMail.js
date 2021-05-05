47818025dc9ee1b3d048ecc2529f9502a85c0e37
Praz <t.prazwal@gmail.com>
2021-05-05 14:19:31 +0200

    dispatch closeSendMessage in SendMail.js to close new message form.

--- src/SendMail.js
+++ src/SendMail.js
@@ -3,6 +3,8 @@ import { Button } from '@material-ui/core';
 import { Close } from '@material-ui/icons';
 import { useForm } from 'react-hook-form';
 import './SendMail.css';
+import { useDispatch } from 'react-redux';
+import { closeSendMessage } from './features/mailSlice';
 
 function SendMail() {
   const {
@@ -12,6 +14,8 @@ function SendMail() {
     formState: { errors },
   } = useForm();
 
+  const dispatch = useDispatch();
+
   const onSubmit = (formData) => {
     console.log(formData);
   }
@@ -20,7 +24,7 @@ function SendMail() {
     <div className="sendMail">
       <div className="sendMail__header">
         <h3>New Message</h3>
-        <Close className="sendMail__close" />
+        <Close className="sendMail__close" onClick={() => dispatch(closeSendMessage())} />
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
