406b34c9e8d3b896ab39f0cabfb960afa2ffd6fb
Praz <t.prazwal@gmail.com>
2021-05-19 16:40:40 +0200

    implement google login feature

--- src/App.js
+++ src/App.js
@@ -8,26 +8,33 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import { useSelector } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 function App() {
-  const sendMessageIsOpen =useSelector(selectSendMessageIsOpen);
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
   return (
     <Router>
-      <div className="app">
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/mail">
-              <Mail />
-            </Route>
-            <Route path="/">
-              <EmailList />
-            </Route>
-          </Switch>
-        </div>
-        {sendMessageIsOpen && <SendMail />}
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/">
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+          {sendMessageIsOpen && <SendMail />}
       </div>
+      )}
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -20,4 +20,4 @@ ReactDOM.render(
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-//254
+//355
